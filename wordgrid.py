798d92f5e4d8b70feda9d5d57c7397eb3475946c
minimus <129208104+plumbusp@users.noreply.github.com>
2026-02-03 15:38:12 +0200

    Worked on diagonal traverse

--- wordgrid.py
+++ wordgrid.py
@@ -1,33 +1,45 @@
 # Counts how many times a given word appears horizontally in a square grid, matching letters from left to right.
+
 class WordFinder:
     def __init__(self):
-        self.grid = [] #
+        self.grid = []
+        self.entriesSet =  set()
+
     def set_grid(self, grid: list):
-        #making a matrix out of grid
-        for entry in grid:
-            self.grid.append(list(entry))
-    # laskee annetun sanan esiintymiskerrat ruudukossa
+        self.grid = grid.copy()
+
     def count(self,word):
-        return self.__searchHorizontal(word) 
+        self.__searchHorizontal(word)
+        print(self.entriesSet)
 
-    def __searchHorizontal(self, word) -> int:
-        count = 0
-        for i in range(0, len(self.grid)):
-            if len(word) != len(self.grid):
-                continue
-            currentWord = ""
-            for j in range(0, len(self.grid[i])):
-                if j >= len(word):
-                    break
-                if self.grid[i][j] == word[j]:
-                    currentWord+=self.grid[i][j]
-                    if currentWord == word:
-                        count +=1
-                        break
+    def __searchHorizontal(self, word):
+        for i in range(0, len(self.grid)): #rows
+            entry = ""
+            entry += self.grid[i][0] # i row, first column
+            for j in range(1,len(self.grid[0])): #rows
+                entry+= self.grid[i][j] # i row, j column
+                if word in entry:
+                    self.entriesSet.add((i+1,j+1)) # new entry on row i, column j (considering offset ny one)
+                    entry = "" # to avoid multiple calculation of the same word
 
-        return count
-
-                
+    def __searchVertically(self, word):
+        for i in range(0, len(self.grid[0])): #columns
+            entry = ""
+            entry += self.grid[0][i] # first row, i column
+            for j in range(1,len(self.grid)): #rows
+                entry+= self.grid[j][i] # j row, i column
+                if word in entry:
+                    print(entry)
+                    self.entriesSet.add((j+1,i+1)) # new entry on row j, column i (considering offset ny one)
+                    entry = "" # to avoid multiple calculation of the same word
+    def __searchDiagonaly(self, startUp = True):
+        rows = len(self.grid) #rows
+        columns = len(self.grid[0]) #columns
+        for d in range(0, rows + columns-1): # for each diagonal there is
+            if d % 2 == 0:
+                r = min(d, rows-1) # diagonal row index is diagonal index if it is less then row amount, otherwise row amount
+                c = d - rows
+                # to do
 
 
 
@@ -41,8 +53,8 @@ if __name__ == "__main__":
     finder.set_grid(grid)
 
     print(finder.count("TIRA")) # 7 
-    print(finder.count("TA")) # 13
-    print(finder.count("RITARI")) # 3
-    print(finder.count("A")) # 8
-    print(finder.count("AA")) # 6
-    print(finder.count("RAITA")) # 0 
+    # print(finder.count("TA")) # 13
+    # print(finder.count("RITARI")) # 3
+    # print(finder.count("A")) # 8
+    # print(finder.count("AA")) # 6
+    # print(finder.count("RAITA")) # 0 
